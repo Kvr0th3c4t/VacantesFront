8a60ec98392f225daf4b56f066297f0b53f78364
Adrian <adrianc.crim@hotmail.com>
2025-05-06 01:44:30 +0200

    adminUsuario cambio de empresaCRUDTable

--- src/app/pages/crudempresas/crudempresas.component.ts
+++ src/app/pages/crudempresas/crudempresas.component.ts
@@ -1,38 +1,96 @@
-import { Component, inject } from '@angular/core';
-import { EmpresasCRUDTableComponent } from "../../components/empresas-crudtable/empresas-crudtable.component";
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { IempresaTable } from '../../interfaces/iempresa-table';
+import { IUsuarioDetalle } from '../../interfaces/iusuario-detalle';
+import Swal from 'sweetalert2';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crudempresas',
   standalone: true,
-  imports: [EmpresasCRUDTableComponent],
+  imports: [RouterLink],
   templateUrl: './crudempresas.component.html',
   styleUrl: './crudempresas.component.css'
 })
 export class CRUDEmpresasComponent {
-		
-      private adminService = inject(AdminService);
+  @Input() usuario!: IUsuarioDetalle;
+  @Output() onEliminar = new EventEmitter<void>();
     
-      
-      arrEmpresas: IempresaTable[];
-      
-      cargando: boolean = true; // Variable para controlar el estado de carga
+  eliminando: boolean;
+  avatarUrl: string;
+  userRole: string;
+  user: IUsuarioDetalle;
+  private adminService = inject(AdminService);
     
-      constructor() {
-        this.arrEmpresas = [];
-      }
-      ngOnInit() {
+  arrEmpresas: IempresaTable[];
+      
+  cargando: boolean = true; // Variable para controlar el estado de carga
+    
+  constructor() {
+    this.arrEmpresas = [];
+    this.eliminando = false;
+    this.avatarUrl = '';
+    this.user = JSON.parse(localStorage.getItem("user")!);
+    this.userRole = this.user.rol || "";
+  }
+  
+  ngOnInit() {
 
-        this.loadEmpresas();
+    this.loadEmpresas();
+    this.generarAvatarAleatorio()
 
+  }
+
+  eliminarEmpresa(idEmpresa: string) {
+        Swal.fire({
+          title: "¿Estás seguro?",
+          text: "Esta acción eliminará la empresa.",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Sí, eliminar",
+          cancelButtonText: "Cancelar",
+          customClass: {
+            confirmButton: "btn btn-secondary",
+            cancelButton: "btn btn-secondary ms-3",
+          },
+          buttonsStyling: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.adminService.eliminarEmpresa(idEmpresa).subscribe(() => {
+              Swal.fire({
+                icon: "success",
+                title: "Eliminada",
+                text: "Tu solicitud ha sido eliminada.",
+                confirmButtonText: "Aceptar",
+                customClass: {
+                  confirmButton: "btn btn-secondary",
+                },
+                buttonsStyling: false,
+              });
+    
+              this.eliminando = true;
+    
+              // Esperamos a que termine la animación antes de avisar al padre
+              setTimeout(() => {
+                this.onEliminar.emit();
+                window.location.reload();
+              }, 300); // duración igual a la del CSS
+            });
+          }
+        });
       }
 
-      //Empresa
-      loadEmpresas() {
-        this.adminService.getAllEmpresasTable().subscribe((data: IempresaTable[]) => {
-          this.arrEmpresas = data;
-          this.cargando = false;			
-        })
-      }
+  generarAvatarAleatorio(): void {
+    const numeroAleatorio = Math.floor(Math.random() * 6) + 1;
+    
+    this.avatarUrl = `https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava${numeroAleatorio}.webp`;
+  }
+    //Empresa
+    loadEmpresas() {
+      this.adminService.getAllEmpresasTable().subscribe((data: IempresaTable[]) => {
+        this.arrEmpresas = data;
+        this.cargando = false;
+      })
     }
+}
+
